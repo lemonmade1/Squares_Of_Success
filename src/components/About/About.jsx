9bfd18bf87d4a5713f48c586aa853b1282acf6e9
Terrell Lemons <lemonsterrell43@gmail.com>
2020-05-27 11:42:07 -0700

    added faux photos to HOME / ABOUT to show functionality

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,14 +7,14 @@ const About = () => {
     <div className="About-App">
       <div id="base">
         <dl id="gallery">
-          <dt><img src="" alt="Step 1 Thumbnail" /></dt>
-          <dd><img src="" alt="Step 1" />Step 1</dd>
+          <dt><img src="./images/minifruit/22.png" alt="Step 1 Thumbnail" /></dt>
+          <dd><img src="./images/22.png" alt="Step 1" />Step 1</dd>
 
-          <dt><img src="" alt="Step 2 Thumbnail" /></dt>
-          <dd><img src="" alt="Step 2" />Step 2</dd>
+          <dt><img src="./images/minifruit/23.png" alt="Step 2 Thumbnail" /></dt>
+          <dd><img src="./images/23.png" alt="Step 2" />Step 2</dd>
 
-          <dt><img src="" alt="Step 3 Thumbnail" /></dt>
-          <dd><img src="" alt="Step 3" />Step 3</dd>
+          <dt><img src="./images/minifruit/24.png" alt="Step 3 Thumbnail" /></dt>
+          <dd><img src="./images/24.png" alt="Step 3" />Step 3</dd>
         </dl>
       </div>      
     </div>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,9 +8,11 @@ const Home = () => {
       <h1>HOME PAGE</h1>
 
       <div id="card_fan">
-        <img src="" alt="Card 1" />
-        <img src="" alt="Card 2" />
-        <img src="" alt="Card 3" />
+        <img id="card1" src="./images/pix1.png" alt="Card 1" />
+        <img id="card2" src="./images/pix1.png" alt="Card 2" />
+        <img id="card3" src="./images/pix1.png" alt="Card 3" />
+        <img id="card4" src="./images/pix1.png" alt="Card 4" />
+        <img id="card5" src="./images/pix1.png" alt="Card 5" />
       </div>
     </div>
   )
