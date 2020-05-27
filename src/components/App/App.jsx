8f1d71fc426e84467d5c16b39614e8c9245fc2fc
Jay Reginaldo <allmotorjay@gmail.com>
2020-05-27 14:43:21 -0700

    Signin Login pages added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,6 +8,8 @@ import About from '../About/About';
 import Game from '../Game/Game';
 import Home from '../Home/Home';
 import NavBar from '../NavBar/NavBar';
+import LoginPage from '../LoginPage/LoginPage';
+import SignupPage from '../SignupPage/SignupPage';
 
 import './App.css';
 
@@ -21,6 +23,8 @@ function App() {
         <Route path='/' component={Home} exact />
         <Route path='/game' component={Game} />
         <Route path='/about' component={About} />
+        <Route path='/login' component={LoginPage} />
+        <Route path='/signup' component={SignupPage} />
       </div>
     </Router>
   );
