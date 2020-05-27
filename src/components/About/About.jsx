ae6cee617710d34000c3eeea160b4f4449e2e5cc
Terrell Lemons <lemonsterrell43@gmail.com>
2020-05-27 09:11:10 -0700

    added feature to ABOUT page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,18 @@ import './About.css';
 const About = () => {
   return (
     <div className="About-App">
-      <h1>ABOUT PAGE</h1>
+      <div id="base">
+        <dl id="gallery">
+          <dt><img src="" alt="Step 1 Thumbnail" /></dt>
+          <dd><img src="" alt="Step 1" />Step 1</dd>
+
+          <dt><img src="" alt="Step 2 Thumbnail" /></dt>
+          <dd><img src="" alt="Step 2" />Step 2</dd>
+
+          <dt><img src="" alt="Step 3 Thumbnail" /></dt>
+          <dd><img src="" alt="Step 3" />Step 3</dd>
+        </dl>
+      </div>      
     </div>
   )
 }
