b18f04b903daaba9cbe892acdda6cd765fafc802
Jay Reginaldo <allmotorjay@gmail.com>
2020-06-01 14:38:18 -0700

    kobe pics and football

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,9 +8,9 @@ const Home = (props) => {
       <div id='card_fan'>
         <img id='card1' src='./images/Kobe_BB/kobe_1.jpeg' alt='Card 1' />
         <img id='card2' src='./images/Kobe_BB/kobe_2.jpeg' alt='Card 2' />
-        <img id='card3' src='./images/Kobe_BB/kobe_3.png' alt='Card 3' />
-        <img id='card4' src='./images/Kobe_BB/kobe_2.jpeg' alt='Card 4' />
-        <img id='card5' src='./images/Kobe_BB/kobe_1.jpeg' alt='Card 5' />
+        <img id='card3' src='./images/Kobe_BB/kobe4.jpeg' alt='Card 3' />
+        <img id='card4' src='./images/Kobe_BB/kobe5.jpeg' alt='Card 4' />
+        <img id='card5' src='./images/Kobe_BB/kobe6.jpeg' alt='Card 5' />
       </div>
     </div>
   )
