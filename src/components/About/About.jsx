6a49ec19db7c18ab038005df59d7aed59ba13801
Jay Reginaldo <allmotorjay@gmail.com>
2020-06-01 00:05:00 -0700

    css login sign up

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,39 +9,40 @@ const About = () => {
       <div id='base'>
         <dl id='gallery'>
           <dt>
-            <img src='./images/minifruit/22.png' alt='Step 1 Thumbnail' />
-          </dt>
-          <dd>
-            <ReactPlayer
-              playing
-              muted
-              url='https://vimeo.com/291913108'
-              alt='Step 1'
-            />
-              Step 1
-          </dd>
-
-          <dt>
-            <img src='./images/minifruit/23.png' alt='Step 2 Thumbnail' />
+            <img src='./images/minifruit/basketball.png' alt='Step 1 Thumbnail' />
           </dt>
           <dd>
             <ReactPlayer
               playing
               muted
               url='https://vimeo.com/341307338'
-              alt='Step 2'
+              alt='Step 1'
             />
-          Step 2
+              Step 1
           </dd>
 
           <dt>
-            <img src='./images/minifruit/24.png' alt='Step 3 Thumbnail' />
+            <img src='./images/minifruit/football.png' alt='Step 2 Thumbnail' />
           </dt>
           <dd>
             <ReactPlayer
               playing
               muted
               url='https://vimeo.com/235907937'
+              alt='Step 2'
+            />
+          Step 2
+          </dd>
+
+          <dt>
+            <img src='./images/miniFruit/baseball.png' alt='Step 3 Thumbnail' />
+          </dt>
+          <dd>
+            <ReactPlayer
+              playing
+              muted
+              url='https://vimeo.com/144990585'
+              
               alt='Step 3'
             />
           Step 3
